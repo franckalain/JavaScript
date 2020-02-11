8a1dd4c2177bb6fe1c8430e3c0937d7353b87f2d
franckalain <k.franckalain@gmail.com>
2020-02-11 02:29:04 +0000

    dependency injection

--- Angular/formation/src/app/courses.component.ts
+++ Angular/formation/src/app/courses.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import { CoursesService } from './courses.servive';
 
 @Component({
     selector: 'courses',
@@ -14,5 +15,9 @@ import {Component} from '@angular/core';
 
 export class CoursesComponent{
     title = 'List of Courses';
-    courses = ["course1", "course2", "course3"];
+    courses;
+
+    constructor(service: CoursesService){
+        this.courses = service.getCourses();
+    }
 }
