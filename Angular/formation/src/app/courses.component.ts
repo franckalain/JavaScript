48ab2af10593ec9282f90c6bf11eac04b1ecd528
franckalain <k.franckalain@gmail.com>
2020-02-11 03:32:03 +0000

    property binding many ways

--- Angular/formation/src/app/courses.component.ts
+++ Angular/formation/src/app/courses.component.ts
@@ -1,23 +1,17 @@
 import {Component} from '@angular/core';
-import { CoursesService } from './courses.service';
+//import { CoursesService } from './courses.service';
 
 @Component({
     selector: 'courses',
     template: `
     <h2>{{title}}</h2>
-    <ul>
-        <li *ngFor="let course of courses">
-            {{course}}
-        </li>
-    </ul>
+    <h2 [textContent] = "title"></h2>
+    <img [src] = "imageUrl">
+    <img src="{{imageUrl}}">
     `
 })
 
 export class CoursesComponent{
     title = 'List of Courses';
-    courses;
-
-    constructor(service: CoursesService){
-        this.courses = service.getCourses();
-    }
+    imageUrl = "http://lorempixel.com/400/200";
 }
