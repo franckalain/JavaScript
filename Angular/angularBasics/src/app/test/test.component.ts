ec570a614ae66f243ee0ffb0eac059c10ec7a451
datacamp20 <datacamp.att20@gmail.com>
2020-02-26 05:35:52 +0000

    component interaction

--- Angular/angularBasics/src/app/test/test.component.ts
+++ Angular/angularBasics/src/app/test/test.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-test',
@@ -17,6 +17,56 @@ import { Component, OnInit } from '@angular/core';
           <div [class]="successClass">Bonjour</div>
           <h2 [class.text-danger]="hasError">error</h2>
           <h2 [ngClass]="messageClasses">conditions sur les classes</h2>
+          
+          <!--style binding-->
+          <h2 [style.color]="hasError ? 'red' : 'green'">style binding</h2>
+          <h2 [style.color]="highLightColor">style binding 2</h2>
+          <h2 [ngStyle]="titleStyles">style binding 3</h2>
+
+          <!--event binding-->
+          <button (click)="onClick()">Great</button>
+          {{greeting}}
+          <button (click)="greeting='bonjour'">Great2</button>
+
+          <!--template reference variables-->
+          <input #myInput type="text">
+          <button (click)="logMessage(myInput.value)">Log</button>
+          <!--two way binding-->
+          <input [(ngModel)]="nom" type="text">
+          {{nom}}
+
+          <!--ngIf Directive-->
+          <h2 *ngIf="displayName; else elseBlock">Bienvenue</h2>
+          <ng-template #elseBlock>
+            <h2>
+              name is hidden
+            </h2>
+          </ng-template>
+
+          <div *ngIf="displayNom; then thenBlock; else elseBlock"></div>
+          <ng-template #thenBlock>
+            <h2>display nom</h2>
+          </ng-template>
+
+          <ng-template #elseBlock>
+            <h2>hidden nom</h2>
+          </ng-template>
+
+          <!--ngSwitch-->
+          <div [ngSwitch]="color">
+            <div *ngSwitchCase="'red'">you picked red color</div>
+            <div *ngSwitchCase="'blue'">you picked blue color</div>
+            <div *ngSwitchCase="'green'">you picked green color</div>
+            <div *ngSwitchDefault="'red'">picked again</div>
+          </div>
+
+          <!--ngFor-->
+          <div *ngFor="let color of colors; index as i"><h2>{{i}} {{color}}</h2></div>
+          
+          <!--component interaction-->
+          <h2>{{"hello " + myName}}</h2>
+
+          <button (click)="fireEvent()">Send Event</button>
           `,
   styles: 
   [
@@ -41,19 +91,61 @@ export class TestComponent implements OnInit {
   isDisabled = false;
   // class binding
   successClass = "text-success";
-  hasError = true;
+  hasError = false;
   isSpecial = true;
   messageClasses = {
     'text-success': !this.hasError,
     'text-danger': !this.hasError,
     'text-special': this.isSpecial
   }
+  // style binding
+  highLightColor = "orange";
+  titleStyles = {
+    color: 'blue',
+    fontStyles: 'italic'
+  }
 
+  // event binding
+  greeting = "";
+  onClick(){
+    console.log("welcome");
+    this.greeting = "welcome franck";
+  }
+
+  //template reference varriale
+  logMessage(value){
+    console.log(value);
+  }
+
+  // two way binding
+  nom = "";
+
+  // ngIf directive
+  displayName = false;
+
+  displayNom = true;
+
+  // ngSwitch
+  color = "orange";
+
+  //ngFor 
+  colors = ["red", "blue", "green", "yellow"];
+
+  // component interaction: regarder dans app.component pour voir la propriete
+
+  @Input('parentData') myName;
+
+  @Output() public childEvent = new EventEmitter();
+  
   constructor() { }
 
   ngOnInit(): void {
   }
     bonjour(){
       return "Bonjour Mr. " + this.name;
+    }
+
+    fireEvent(){
+      this.childEvent.emit('hey');
     }
 }
